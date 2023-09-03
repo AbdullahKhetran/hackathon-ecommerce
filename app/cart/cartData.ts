8613e83eecf8445cf85597a155bd50fed26459bb
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-03 23:18:57 +0500

    passign origin as parameter

--- app/cart/cartData.ts
+++ app/cart/cartData.ts
@@ -1,9 +1,22 @@
 import { CombinedProduct } from "@/types/products";
 
 export async function getData(uid: string) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/cart?userid=${uid}`)
+    // const res = await fetch(`http://localhost:3000/api/cart?userid=${uid}`)
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    const data: CombinedProduct[] = await res.json()
+
+    return data
+}
+
+export async function getDataWithOrigin(uid: string, origin: string) {
     const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/cart?userid=${uid}`, {
         headers: {
-            "Origin": window.location.origin
+            "Origin": origin
         }
     })
     // const res = await fetch(`http://localhost:3000/api/cart?userid=${uid}`)
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer/Footer"
 import Copyright from "@/components/Footer/Copyright"
 import Navbar from "@/components/Navbar/Navbar"
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { getData } from "@/app/cart/cartData"
+import { getData, getDataWithOrigin } from "@/app/cart/cartData"
 import { useEffect, useState } from "react"
 import { CombinedProduct, MyProduct } from "@/types/products";
 import { MinusIcon, PlusIcon, ShoppingCart, Trash2 } from "lucide-react"
@@ -60,7 +60,10 @@ export default function Home() {
     useEffect(() => {
         async function fetchData() {
             try {
-                const fetchedData = await getData(userid);
+                // const fetchedData = await getData(userid);
+                const origin = window.location.origin
+                console.log("Origin is ", origin)
+                const fetchedData = await getDataWithOrigin(userid, origin);
                 // console.log(fetchedData)
                 // console.log("fetched data is an array?", Array.isArray(fetchedData))
 
