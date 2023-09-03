0e485aae9f6020ee4d7a7b6b1a7e58ea42923f46
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-03 23:04:11 +0500

    added origin in headers

--- app/cart/cartData.ts
+++ app/cart/cartData.ts
@@ -1,7 +1,11 @@
 import { CombinedProduct } from "@/types/products";
 
 export async function getData(uid: string) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/cart?userid=${uid}`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/cart?userid=${uid}`, {
+        headers: {
+            "Origin": window.location.origin
+        }
+    })
     // const res = await fetch(`http://localhost:3000/api/cart?userid=${uid}`)
 
     if (!res.ok) {
