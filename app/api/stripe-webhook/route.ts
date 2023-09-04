f5354fa985cf856e7019c89cb534d5ff76157580
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-05 03:55:28 +0500

    using buffer for stripe signature body

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -1,20 +1,102 @@
+// import Stripe from "stripe";
+// import { db, cartTable } from "@/lib/drizzle";
+// import { eq } from "drizzle-orm";
+// import { NextResponse } from "next/server";
+
+
+// export async function POST(req: Request, res: any) {
+
+//     const webHookSecret = process.env.STRIPE_WEBHOOK_SIGNING_SECRET as string
+
+//     try {
+
+//         const stripe = new Stripe(
+//             process.env.STRIPE_SECRET_KEY as string,
+//             {
+//                 apiVersion: '2023-08-16',
+//             }
+//         )
+
+//         let event: Stripe.Event
+
+//         try {
+
+//             // got this from docs https://github.com/vercel/next.js/blob/canary/examples/with-stripe-typescript/app/api/webhooks/route.ts
+//             event = stripe.webhooks.constructEvent(
+//                 await (await req.blob()).text(),
+//                 req.headers.get('stripe-signature') as string,
+//                 webHookSecret
+//             )
+
+//         } catch (err) {
+//             const errorMessage = err instanceof Error ? err.message : 'Unknown error'
+//             // On error, log and return the error message.
+//             if (err! instanceof Error) console.log(err)
+//             console.log(`❌ Error message: ${errorMessage}`)
+//             return NextResponse.json(
+//                 { message: `Webhook Error: ${errorMessage}` },
+//                 { status: 400 }
+//             )
+//         }
+
+
+//         if (event.type === "checkout.session.completed") {
+//             const session = event.data.object;
+//             // @ts-ignore
+//             const customerData = await stripe.customers.retrieve(session.customer)
+//             // @ts-ignore
+//             const userId = customerData.metadata.userId; // userid that was used to register customer
+
+//             await db.delete(cartTable).where(eq(cartTable.userid, userId));
+
+//             return NextResponse.json(
+//                 { message: "Payment Confirmation Router Reciept" },
+//                 { status: 200 }
+//             );
+
+
+//         } else {
+//             res.setHeader("Allow", "POST");
+//         }
+//     } catch (error) {
+//         console.log("Error in webhook", error);
+//         return;
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
 import Stripe from "stripe";
 import { db, cartTable } from "@/lib/drizzle";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
+import { buffer } from "micro"
 
 
-export async function POST(req: Request, res: any) {
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+
+export async function POST(req: any, res: any) {
 
     const webHookSecret = process.env.STRIPE_WEBHOOK_SIGNING_SECRET as string
 
     try {
+        const requestBuffer = await buffer(req)
 
         const stripe = new Stripe(
             process.env.STRIPE_SECRET_KEY as string,
-            {
-                apiVersion: '2023-08-16',
-            }
+            { apiVersion: '2023-08-16', }
         )
 
         let event: Stripe.Event
@@ -23,7 +105,7 @@ export async function POST(req: Request, res: any) {
 
             // got this from docs https://github.com/vercel/next.js/blob/canary/examples/with-stripe-typescript/app/api/webhooks/route.ts
             event = stripe.webhooks.constructEvent(
-                await (await req.blob()).text(),
+                requestBuffer.toString(), // Stringify the request for the Stripe library
                 req.headers.get('stripe-signature') as string,
                 webHookSecret
             )
@@ -64,3 +146,5 @@ export async function POST(req: Request, res: any) {
     }
 
 }
+
+
