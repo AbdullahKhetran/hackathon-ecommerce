1ef1103e757df4ffe27e70632c3f828823d54b39
Abdullah Khetran <khetranabdullah@gmail.com>
2023-09-08 03:59:06 +0500

    fixed images responsiveness

--- components/Product/Images.tsx
+++ components/Product/Images.tsx
@@ -12,28 +12,31 @@ type Props = {
 export default function ImageCarousel({ matchingProduct }: Props) {
     const [index, setIndex] = useState(0)
     return (
-        <div className="flex gap-8 ">
-            <div className="flex flex-col gap-2 justify-between">
+        <div className="grow flex gap-8 lg:min-w-[65%] ">
+            <div className="flex flex-col gap-2 ">
                 {matchingProduct?.images.map((image, i) => (
                     <Image
                         key={i}
                         src={urlFor(image).url()}
                         alt="Secondary Image"
-                        width={150}
-                        height={150}
+                        width={278}
+                        height={296}
                         onMouseEnter={() => setIndex(i)}
-                        className="hover:cursor-pointer"
+                        className="hover:cursor-pointer aspect-square w-[50px] xs:w-[80px] sm:w-[100px] md:w-[120px] "
                     />
                 ))}
             </div>
 
-            <div className="w-full h-full">
+            <div className="w-2/3 ">
+
                 <Image
                     src={urlFor(matchingProduct.images[index]).url()}
                     alt={matchingProduct.name}
-                    width={500}
-                    height={500}
+                    width={278}
+                    height={296}
+                    style={{ width: "100%", height: "auto" }}
                 />
+
             </div>
         </div>
     )
